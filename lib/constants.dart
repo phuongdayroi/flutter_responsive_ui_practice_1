e6d2b5a10fbef97d35bf0a40fba18c54c58e01ba
Phuong Tran <nhatphuongb1@gmail.com>
2021-07-31 15:58:23 +0700

    init project

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ThemeConstants{
+static const double kPadding = 10.0;
+static const Color purpleDark = Color(0xff3c096c);
+static const Color purpleLight = Color(0xff5a189a);
+static const Color orangeLight = Color(0xfff8961e);
+static const Color orangeDark = Color(0xfff3722c);
+static const Color redLight = Color(0xffef233c);
+static const Color redDark = Color(0xffd90429);
+static const Color blueLight = Color(0xff0077b6);
+static const Color blueDark = Color(0xff023e8a);
+static const Color greenLight = Color(0xff57cc99);
+static const Color greenDark = Color(0xff38a3a5);
+}
